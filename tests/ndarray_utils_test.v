eed71922c9c75c64b2385131688bd61eade0f317
Aria Ghora Prabono <hello@ghora.net>
2021-12-31 00:43:59 +0700

    Update ndarray_utils_test.v

--- tests/ndarray_utils_test.v
+++ tests/ndarray_utils_test.v
@@ -40,19 +40,25 @@ fn test_get_view_linear_data_simple_after_one_slicing() {
 }
 
 fn test_get_view_linear_data_simple_after_two_slicings() {
-	eprintln('Not implemented yet')
-	assert false
+	a := ndarray.create_ndarray([1.0, 2.0, 3.0, 4.0], 4)
+	mut slice := a.slice([1, 3])
+	assert ndarray.get_view_linear_data(slice) == [2.0, 4.0]
+	slice = slice.slice([1])
+	assert ndarray.get_view_linear_data(slice) == [4.0]
 }
 
 fn test_get_view_linear_data_2d_after_one_slicing() {
 	a := ndarray.create_ndarray([1.0, 2.0, 3.0, 4.0], 2, 2)
-	slice := a.slice([0], [1, 3])
+	slice := a.slice(ndarray.all, [1])
 	assert ndarray.get_view_linear_data(slice) == [2.0, 4.0]
 }
 
 fn test_get_view_linear_data_2d_after_two_slicings() {
-	eprintln('Not implemented yet')
-	assert false
+	a := ndarray.create_ndarray([1.0, 2.0, 3.0, 4.0], 2, 2)
+	mut slice := a.slice(ndarray.all, [1])
+	assert ndarray.get_view_linear_data(slice) == [2.0, 4.0]
+	slice = slice.slice([0])
+	assert ndarray.get_view_linear_data(slice) == [2.0]
 }
 
 fn test_get_view_linear_data_3d_after_one_slicing() {
@@ -62,6 +68,9 @@ fn test_get_view_linear_data_3d_after_one_slicing() {
 }
 
 fn test_get_view_linear_data_3d_after_two_slicings() {
-	eprintln('Not implemented yet')
-	assert false
+	a := ndarray.create_ndarray([]f64{len: 12, init: it}, 3, 2, 2)
+	mut slice := a.slice([0, 2], [0])
+	assert ndarray.get_view_linear_data(slice) == [0.0, 1.0, 8.0, 9.0]
+	slice = slice.slice([0])
+	assert ndarray.get_view_linear_data(slice) == [0.0, 1.0]
 }
